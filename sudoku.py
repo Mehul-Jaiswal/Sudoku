a06f8316176d6ef9436958cef0a204c83d84b2f7
Mehul-Jaiswal <87316002+Mehul-Jaiswal@users.noreply.github.com>
2022-07-27 22:53:11 -0700

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,6 +8,7 @@ board=[
     [0,6,0,0,0,0,2,8,0],
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,8,0,0,7,4]]
+
 def print_board(bo):
     for i in range(len(bo)):
         if i%3 == 0 and i!=0:
@@ -19,12 +20,14 @@ def print_board(bo):
                 print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ",end="")
+                
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j)
     return None
+
 def valid(bo, num, pos):
     for i in range(len(bo[0])):#checking row
         if bo[pos[0]][i] == num and pos[1]!= i:
@@ -39,6 +42,7 @@ def valid(bo, num, pos):
             if bo[i][j] == num and  (i,j) != pos:
                 return False
     return True
+
 def solve(bo):
     find = find_empty(bo)
     if not find:
@@ -52,6 +56,7 @@ def solve(bo):
                 return True
             bo[row][col]=0
     return False
+
 print_board(board)
 solve(board)
 print("Solving")
